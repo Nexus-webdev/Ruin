53942d7bc2f972f0a81f7acaffd67101f654cefc
Nexus-webdev <welshsam748@gmail.com>
2025-10-04 17:09:08 -0400

    Update syntax.js

--- syntax.js
+++ syntax.js
@@ -2301,7 +2301,7 @@ $.struct('NeuralNetwork', {
   if (levels)
   {
    for (let lvl of levels)
-   this.levels.push(typeof lvl == 'string' ? $.Level.parse(lvl) : lvl.copy ? lvl.copy() : lvl);
+   this.levels.push(typeof lvl == 'string' ? $.Level.parse(lvl) : lvl.copy ? lvl.copy() : $.Level.stringify($.Level.parse(lvl)));
    
    return;
   }
