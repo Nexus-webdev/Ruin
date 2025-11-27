5421d8b00d3c8c4d6846fd9084c000c3aaa869c0
Nexus-webdev <welshsam748@gmail.com>
2025-11-27 19:30:01 -0400

    Update syntax.js

--- syntax.js
+++ syntax.js
@@ -572,7 +572,7 @@ self.$ = ({
     
     const previousLine = t.value.substring(0, t.selectionStart).split('\n').pop();
     const indentation = previousLine.match(/^\s*/)[0];
-    if (InBrackets() || InBrackets('[', ']')))
+    if (InBrackets() || InBrackets('[', ']'))
     {
      t.value = `${t.value.substring(0, t.selectionStart)}
 ${indentation} 
