68aae422ae8914096121f8016d38f5b52ee63024
Nexus-webdev <welshsam748@gmail.com>
2025-11-27 15:31:52 -0400

    Update
    Fixed the service workers. I didn't add fetch listeners. I'M AN IDIOT!

--- service_workers/manager.js
+++ service_workers/manager.js
@@ -1,11 +1,29 @@
-self.addEventListener('install', event => {
- event.waitUntil(caches.open('manager_cache_v1').then(cache => {
-  cache.addAll([
-   '/Ruin/',
-   '/Ruin/Manager.html',
-   '/Ruin/syntax.js',
-   
-   '/Ruin/icons/manager.png',
-  ]);
+const cache_name = 'manager_cache_v1';
+const assets = [
+ '/Ruin/',
+ '/Ruin/Manager.html',
+ '/Ruin/syntax.js',
+ 
+ '/Ruin/icons/manager.png',
+];
+
+"Install event: cache files";
+self.addEventListener('install', e => {
+ e.waitUntil(caches.open(cache_name).then(cache => {
+  return cache.addAll(assets);
+ }));
+});
+
+"Activate event: cleanup old caches";
+self.addEventListener('activate', e => {
+ e.waitUntil(caches.keys().then(keys =>
+  Promise.all(keys.filter(k => k != cache_name).map(k => caches.delete(k)))
+ ));
+});
+
+"Fetch event: serve cached files";
+self.addEventListener('fetch', e => {
+ e.respondWith(caches.match(e.request).then(response => {
+  return response || fetch(e.request);
  }));
 });
--- service_workers/output.js
+++ service_workers/output.js
@@ -1,13 +1,34 @@
+const cache_name = 'rre_cache_v1';
+const assets = [
+ '/Ruin/',
+ '/Ruin/Output.html',
+ '/Ruin/syntax.js',
+ '/Ruin/page.js',
+ 
+ '/Ruin/bootstrapper.$',
+ '/Ruin/addons/foxx@dom.$',
+ '/Ruin/addons/foxx.$',
+ 
+ '/Ruin/icons/rre.png',
+];
+
+"Install event: cache files";
 self.addEventListener('install', e => {
- e.waitUntil(caches.open('rre_cache_v1').then(cache => {
-  cache.addAll([
-   '/Ruin/',
-   '/Ruin/Output.html',
-   '/Ruin/syntax.js',
-   '/Ruin/page.js',
-   
-   '/Ruin/bootstrapper.$',
-   '/Ruin/icons/rre.png',
-  ]);
+ e.waitUntil(caches.open(cache_name).then(cache => {
+  return cache.addAll(assets);
+ }));
+});
+
+"Activate event: cleanup old caches";
+self.addEventListener('activate', e => {
+ e.waitUntil(caches.keys().then(keys =>
+  Promise.all(keys.filter(k => k != cache_name).map(k => caches.delete(k)))
+ ));
+});
+
+"Fetch event: serve cached files";
+self.addEventListener('fetch', e => {
+ e.respondWith(caches.match(e.request).then(response => {
+  return response || fetch(e.request);
  }));
 });
