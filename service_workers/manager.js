113ed4109a59e91c37fb4a077bfe8644cc8d1b1f
Nexus-webdev <welshsam748@gmail.com>
2025-11-27 13:01:03 -0400

    Update

--- service_workers/manager.js
+++ service_workers/manager.js
@@ -1,13 +1,11 @@
 self.addEventListener('install', event => {
- event.waitUntil(
-  caches.open('v1').then(cache => {
-   cache.addAll([
-    '/Ruin/',
-    '/Ruin/Manager.html',
-    '/Ruin/syntax.js',
-    
-    '/Ruin/icons/manager.png',
-   ]);
-  })
- );
+ event.waitUntil(caches.open('v1').then(cache => {
+  cache.addAll([
+   '/Ruin/',
+   '/Ruin/Manager.html',
+   '/Ruin/syntax.js',
+   
+   '/Ruin/icons/manager.png',
+  ]);
+ }));
 });
--- service_workers/output.js
+++ service_workers/output.js
@@ -1,18 +1,16 @@
 self.addEventListener('install', event => {
- event.waitUntil(
-  caches.open('v1').then(cache => {
-   cache.addAll([
-    '/Ruin/',
-    '/Ruin/Output.html',
-    '/Ruin/syntax.js',
-    '/Ruin/page.js',
-    
-    '/Ruin/bootstrapper.$',
-    '/Ruin/addons/foxx@dom.$',
-    '/Ruin/addons/foxx.$',
-    
-    '/Ruin/icons/rre.png',
-   ]);
-  })
- );
+ event.waitUntil(caches.open('v1').then(cache => {
+  cache.addAll([
+   '/Ruin/',
+   '/Ruin/Output.html',
+   '/Ruin/syntax.js',
+   '/Ruin/page.js',
+   
+   '/Ruin/bootstrapper.$',
+   '/Ruin/addons/foxx@dom.$',
+   '/Ruin/addons/foxx.$',
+   
+   '/Ruin/icons/rre.png',
+  ]);
+ }));
 });
