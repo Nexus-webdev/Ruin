f730c6ce45c2abe6e0f3910ae6da165e1467c099
Nexus-webdev <welshsam748@gmail.com>
2025-12-02 14:37:03 -0400

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -25,8 +25,8 @@ self.addEventListener('install', e => {
 
 "Fetch event: serve cached files";
 self.addEventListener('fetch', e => {
- const [request] = e.request.split('?');
- e.respondWith(caches.match(request).then(response => {
+ console.log(e.request);
+ e.respondWith(caches.match(e.request).then(response => {
   return response || fetch(e.request);
  }));
 });
