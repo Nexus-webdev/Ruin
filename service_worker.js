1cb1950b255c07f2f55be40ccef6e7e1cfc7ca27
Simon <welshsam748@gmail.com>
2026-02-08 23:31:29 -0400

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -32,38 +32,19 @@ self.addEventListener('activate', event => {
  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key != CACHE_NAME).map(key => caches.delete(key)))));
 });
 
-"Fetch event: serve cached files ignoring query params";
+"Fetch event: serve cached files";
 self.addEventListener('fetch', event => {
  const url = new URL(event.request.url);
+ const cache_key = url.pathname;
  
- // For documents, ignore query when matching cache
- const cacheKey = event.request.destination == 'document' ? url.pathname : url.pathname;
- 
- event.respondWith(caches.match(cacheKey).then(cached => {
-  if (cached)
-  {
-   // Update cache in background
-   fetch(event.request).then(response => {
-    if (response.ok)
-    {
-     caches.open(CACHE_NAME).then(cache => cache.put(cacheKey, response.clone()));
-     offline = false;
-    }
-   }).catch(e => {
-    if (!offline)
-    {
-     console.log('User Offline, falling back to cached resources');
-     console.log(`%cEncountered Error: ${e.message}`, 'color: red');
-    }
-    
-    offline = true;
-   });
-   
-   return cached;
-  }
+ event.respondWith(caches.match(cache_key).then(cached => {
+  console.log('Destination: ', event.request.destination);
+  console.log('Cached Key: ', cached_key);
+  console.log('Cached: ', cached);
+  if (cached) return cached;
   
   return fetch(event.request).catch(() => {
-   if (event.request.destination == 'document') return caches.match(cacheKey) || caches.match('/Ruin/index.html');
+   if (event.request.destination == 'document') return caches.match(cache_key) || caches.match('/Ruin/index.html');
   });
  }));
 });
