9d5f1e196249e95027f66f0f8618c28224cf9c29
Simon <welshsam748@gmail.com>
2026-02-10 10:41:39 -0400

    Update

--- service_worker.js
+++ service_worker.js
@@ -1,50 +1,68 @@
-const CACHE_NAME = 'ruin_cache_v1';
-let offline = false;
+// Not my code.
+
+const CACHE_NAME = 'ruin_cache_v4';
 const ASSETS = [
- '/Ruin/',
- '/Ruin/index.html',
- '/Ruin/Output.html',
- '/Ruin/Manager.html',
- '/Ruin/Editor.html',
- '/Ruin/file_opener.html',
-
- '/Ruin/manager-styles.css',
- '/Ruin/editor-styles.css',
- '/Ruin/output-styles.css',
-
- '/Ruin/syntax.js',
- '/Ruin/page.js',
-
- '/Ruin/addons/dom_flux.$',
- '/Ruin/bootstrapper.$',
- '/Ruin/flux.$',
-
- '/Ruin/icons/rre.png',
+  '/Ruin/',
+  '/Ruin/index.html',
+  '/Ruin/Output.html',
+  '/Ruin/Manager.html',
+  '/Ruin/Editor.html',
+  '/Ruin/file_opener.html',
+  '/Ruin/manager-styles.css',
+  '/Ruin/editor-styles.css',
+  '/Ruin/output-styles.css',
+  '/Ruin/syntax.js',
+  '/Ruin/page.js',
+  '/Ruin/addons/dom_flux.$',
+  '/Ruin/bootstrapper.$',
+  '/Ruin/flux.$',
+  '/Ruin/icons/rre.png',
 ];
 
-"Install event: cache files";
+// Install: cache core assets
 self.addEventListener('install', event => {
- event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-"Activate event: cleanup old caches";
+// Activate: clean old caches
 self.addEventListener('activate', event => {
- event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key != CACHE_NAME).map(key => caches.delete(key)))));
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
 });
 
-"Fetch event: serve cached files";
+// Fetch: serve cache instantly, update in background
 self.addEventListener('fetch', event => {
- const url = new URL(event.request.url);
- const cache_key = url.pathname;
- 
- event.respondWith(caches.open(CACHE_NAME).then(cache => cache.match(cache_key).then(cached => {
-  "Always attempt to update in the background. If network fails, fall back to cached";
-  const promise = fetch(event.request).then(response => {
-   if (response && response.ok) cache.put(cache_key, response.clone());
-   return response;
-  }).catch(x => cached);
+  const url = new URL(event.request.url);
 
-  "If we have cached content, serve it immediately. Otherwise, we wait on the fetch promise";
-  return cached || promise;
- })));
+  // For documents, ignore query params when caching
+  const cacheKey = event.request.destination === 'document'
+    ? url.pathname
+    : url.pathname + url.search;
+
+  event.respondWith(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.match(cacheKey).then(cached => {
+        // Kick off network fetch in background
+        const networkFetch = fetch(event.request).then(response => {
+          if (response && response.ok) {
+            cache.put(cacheKey, response.clone());
+          }
+          return response;
+        }).catch(() => cached);
+
+        // If we have cached content, return it immediately
+        if (cached) {
+          return cached;
+        }
+
+        // Otherwise, wait for network
+        return networkFetch;
+      })
+    )
+  );
 });
